89e2085406b2acc30ab5416cb34e705f8a0141ba
zacrogers <38870655+zacrogers@users.noreply.github.com>
2020-01-03 10:01:12 +1300

    Update servo_joystick.ino

--- servo_joystick.ino
+++ servo_joystick.ino
@@ -1,4 +1,7 @@
 #include <Servo.h>
+#include "my_structs.h"
+
+uint8_t play_btn_pin = 3;
 
 Servo servo_x;
 Servo servo_y;
@@ -6,9 +9,15 @@ Servo servo_y;
 JoyStick js = {.x_pin   = A0, 
                .y_pin   = A1, 
                .btn_pin = 5};
-Pos pos = {0};
 
-void setup
+Pos curr_pos = {.x = 90, .y = 90};
+
+Pos path[5] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
+
+Mode      curr_mode  = JOYSTICK;
+PlayState play_state = STOPPED;
+
+void setup()
 {
     servo_x.attach(8); 
     servo_y.attach(9);
@@ -17,23 +26,92 @@ void setup
     pinMode(js.y_pin, INPUT);
     pinMode(js.btn_pin, INPUT);
     
-    init_pos();
+    pinMode(play_btn_pin, INPUT);
+    
+    set_pos(curr_pos);
 }
 
-void loop
+void loop()
 {
-    pos.x = analogRead(js.x_pin);
-    pos.y = analogRead(js.y_pin);
+    poll_buttons();
     
-    pos.x = map(pos.x, 0, 1023, 0, 180);
-    pos.y = map(pos.y, 0, 1023, 0, 180);
-    
-    servo_x.write(pos.x);
-    servo_y.write(pos.y);
+    switch(curr_mode)
+    {
+        case JOYSTICK:
+            read_joystick();
+            break;
+            
+        case PLAY:
+            play_routine();
+            break;          
+            
+        case FOLLOW:
+            break;            
+    }
 }
 
-void init_pos()
+Pos read_joystick(void)
 {
-    servo_x.write(90);
-    servo_y.write(90);
+    curr_pos.x = analogRead(js.x_pin);
+    curr_pos.y = analogRead(js.y_pin);
+    
+    curr_pos.x = map(curr_pos.x, 0, 1023, 0, 180);
+    curr_pos.y = map(curr_pos.y, 0, 1023, 0, 180);
+    
+    set_pos(curr_pos);
 }
+
+void play_routine(void)
+{
+    if(play_state == PLAYING)
+    {
+        for(int i = 0; i < 5; ++i)
+        {
+            set_pos(path[i]);
+            delay(500);
+        }
+    }
+}
+
+void set_pos(Pos p)
+{
+    servo_x.write(p.x);
+    servo_y.write(p.y);
+}
+
+void poll_buttons(void)
+{
+    /* Use joystick button to cycle through modes */
+    if(digitalRead(js.btn_pin) == HIGH)
+    {
+        cycle_mode();
+    }
+    
+    if(digitalRead(play_btn_pin) == HIGH)
+    {
+        toggle_playstate();
+    }
+}
+
+void toggle_playstate(void)
+{
+    if(play_state == PLAYING)
+        play_state == STOPPED;
+    
+    else if(play_state == STOPPED)
+        (play_state == PLAYING);    
+}
+
+void cycle_mode(void)
+{
+    if(current_mode == JOYSTICK)
+        current_mode == FOLLOW
+        
+    else if(current_mode == FOLLOW)
+        current_mode == PLAY
+        
+    else if(current_mode == PLAY)
+        current_mode == JOYSTICK        
+}
+
+
